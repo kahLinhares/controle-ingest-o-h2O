92d18d81c8d87894eef729f5ecb198ded910aeed
arimateiajunior.tic@gmail.com <arimateiajunior.tic@gmail.com>
2024-06-23 15:54:04 -0300

    Ajustando teste Calcular

--- backend/tests/test_calcular.py
+++ backend/tests/test_calcular.py
@@ -13,17 +13,22 @@ def test_calcular(client):
     ]
     for payload in test_cases:
         response = client.post("/calcular", json=payload)  # Envia payload como JSON
-
+    
         response_json = response.get_json()  # Extrai JSON da resposta
-
-        ml_agua = 0
-
-        if payload["idade_grupo"] == "adulto":
-            ml_agua = 35
-        elif payload["idade_grupo"] == "crianca":
-            ml_agua = 50
-
-        expected_total = payload["peso"] * ml_agua
-
-        assert 'total' in response_json  # Verifica se 'total' está presente no JSON
-        assert response_json['total'] == expected_total
+        
+        if 'error' in response_json:
+            assert response.status_code == 400  # Verifica o código de status 400 para erro
+            assert 'total' not in response_json  # Verifica se não há 'total' se há um erro
+            assert response_json['error'] == 'Peso deve ser maior que 0'  # Verifica a mensagem de erro específica
+        else:
+            ml_agua = 0
+    
+            if payload["idade_grupo"] == "adulto":
+                ml_agua = 35
+            elif payload["idade_grupo"] == "crianca":
+                ml_agua = 50
+    
+            expected_total = payload["peso"] * ml_agua
+    
+            assert 'total' in response_json  # Verifica se 'total' está presente no JSON
+            assert response_json['total'] == expected_total  # Verifica se o valor calculado está correto
