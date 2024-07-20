5af10a6f97d80430eb8ebeffbe299ebd60aa9ec3
Arimatéia Júnior - UNIFOR <arimateia.junior@edu.unifor.br>
2024-07-20 09:30:38 -0300

    Incluindo teste TDD

--- tdd/test_calc-gravida.py
+++
@@ -1,7 +0,0 @@
-# Test_calculadora_agua.py usando TDD
-def test_calculo_agua_para_gravidas():
-    # Assumindo que a função `calcular_agua_para_gravidas` ainda não existe
-    from calculadora_agua import calcular_agua_para_gravidas
-
-    # Teste básico: 2 litros por dia mais 300 ml para grávidas
-    assert calcular_agua_para_gravidas(70) == (70 * 35) + 0.3
